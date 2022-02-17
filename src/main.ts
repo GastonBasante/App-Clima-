d9c11db9ca966d190dfbae7535ddd297d6703786
Gaston Basante <gastonbasante@gmail.com>
2022-02-17 02:02:01 -0300

    main:add location

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,13 @@ import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2FzdG9uYmFzYW50ZSIsImEiOiJja3llZm5kNm4wZXloMndtbGt3YXB3aXd3In0.rK7XehnBTAyYdWUxDAalCg';
 
 
+if(!navigator.geolocation){
+  alert('Tu navegador no soporta la Geolocalizacion')
+  throw new Error('Tu navegador no soporta la Geolocalizacion')
+  
+}
+
+
 if (environment.production) {
   enableProdMode();
 }
